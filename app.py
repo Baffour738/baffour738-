dfed893fa421b1de531a30ae728a645ad691f38e
Akowuah Addo Baffour <Akowuah Addo Baffour>
2024-12-23 20:24:02 +0000

    hello

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-def addFunction(x, y):
-    return x + y
+while True:
+    try:
+        number = int(input("Enter a number to check whether even: "))
+        if number % 2 == 0:
+            print(f"{number} is even")
+
+        else:
+            print(f"{number} is odd")
+
+    except:
+        print("Please enter a number only.")
